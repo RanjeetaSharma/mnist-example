729975e3222f1dda4a8c2b0009df8a2bd564b739
ranjeta <sharma.73@iitj.ac.in>
2021-09-02 21:53:12 +0530

    requirements.txt file created

--- mnist-example/plot_digits_classification.py
+++ mnist-example/plot_digits_classification.py
@@ -1,6 +1,4 @@
-"""
-================================
-Recognizing hand-written digits
+ecognizing hand-written digits
 ================================
 
 This example shows how scikit-learn can be used to recognize images of
@@ -40,8 +38,7 @@ for ax, image, label in zip(axes, digits.images, digits.target):
         ax.set_axis_off()
             ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
                 ax.set_title('Training: %i' % label)
-
-                ###############################################################################
+##############################################################################
                 # Classification
                 # --------------
                 #
@@ -65,20 +62,20 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 
                 # Split data into 50% train and 50% test subsets
                 X_train, X_test, y_train, y_test = train_test_split(
-                            data, digits.target, test_size=0.5, shuffle=False)
+                    data, digits.target, test_size=0.5, shuffle=False)
 
-                # Learn the digits on the train subset
-                clf.fit(X_train, y_train)
+                    # Learn the digits on the train subset
+                    clf.fit(X_train, y_train)
 
-                # Predict the value of the digit on the test subset
-                predicted = clf.predict(X_test)
+                    # Predict the value of the digit on the test subset
+                    predicted = clf.predict(X_test)
 
-                ###############################################################################
-                # Below we visualize the first 4 test samples and show their predicted
-                # digit value in the title.
+                    ###############################################################################
+                    # Below we visualize the first 4 test samples and show their predicted
+                    # digit value in the title.
 
-                _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-                for ax, image, prediction in zip(axes, X_test, predicted):
+                    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+                    for ax, image, prediction in zip(axes, X_test, predicted):
                         ax.set_axis_off()
                             image = image.reshape(8, 8)
                                 ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
@@ -89,15 +86,15 @@ for ax, image, label in zip(axes, digits.images, digits.target):
                                     # the main classification metrics.
 
                                     print(f"Classification report for classifier {clf}:\n"
-                                                  f"{metrics.classification_report(y_test, predicted)}\n")
+                                          f"{metrics.classification_report(y_test, predicted)}\n")
 
-                                    ###############################################################################
-                                    # We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-                                    # true digit values and the predicted digit values.
+                                          ###############################################################################
+                                          # We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
+                                          # true digit values and the predicted digit values.
 
-                                    disp = metrics.plot_confusion_matrix(clf, X_test, y_test)
-                                    disp.figure_.suptitle("Confusion Matrix")
-                                    print(f"Confusion matrix:\n{disp.confusion_matrix}")
+                                          disp = metrics.plot_confusion_matrix(clf, X_test, y_test)
+                                          disp.figure_.suptitle("Confusion Matrix")
+                                          print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
-                                    plt.show()
+                                          plt.show()
 
