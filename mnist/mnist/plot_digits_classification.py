43edd35b3d17ea55187fd2d7bca487afd5fa8c4f
RanjeetaSharma <61404441+RanjeetaSharma@users.noreply.github.com>
2021-09-18 11:55:54 +0530

    Update plot_digits_classification.py

--- mnist/mnist/plot_digits_classification.py
+++ mnist/mnist/plot_digits_classification.py
@@ -65,24 +65,23 @@ n_samples = len(digits.images)
 print("Image size is:")
 print(digits.images[0].shape)
 
+resize_imgs=[]
 rescale_val = [1, 2, 3]
 test_split = [0.2, 0.3, 0.4]
 gamma_val= [1, 0.1, 0.01, 0.001, 10]
+
 print("Image Size  Train-Test Split  Gamma val  Accuracy\t\t  F1 Score")
 for val in rescale_val:
-    resize_imgs=[]
     for img  in digits.images:
         resize_imgs.append(rescale(img,val, anti_aliasing=False))
     for test in test_split:
-        
-
         resize_imgs = np.array(resize_imgs)
         data = resize_imgs.reshape((n_samples, -1))
         for g in gamma_val:
             # Create a classifier: a support vector classifier
             clf = svm.SVC(gamma= g)
 
-            # Split data into 50% train and 50% test subsets
+            # Split data into train and test subsets
             X_train, X_test, y_train, y_test = train_test_split(
                 data, digits.target, test_size=test, shuffle=False)
 
@@ -99,7 +98,7 @@ for val in rescale_val:
             _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
             for ax, image, prediction in zip(axes, X_test, predicted):
                 ax.set_axis_off()
-                image = image.reshape(resized_images[0].shape)
+                image = image.reshape(resize_imgs[0].shape)
                 ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
                 ax.set_title(f'Prediction: {prediction}')
 
@@ -124,6 +123,7 @@ for val in rescale_val:
 
             plt.show()
 
+## Finding the best estimatior
 param_grid = { 'gamma': [1, 0.1, 0.01, 0.001, 10]}
 # Make grid search classifier
 
@@ -134,4 +134,3 @@ clf_grid.fit(X_train, y_train)
 # clf = grid.best_estimator_()
 
 print("Best Estimators:\n", clf_grid.best_estimator_)
-
