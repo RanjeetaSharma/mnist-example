685ca993359898e2fccae9ca81d0553f2dedbbf8
ranjeta <sharma.73@iitj.ac.in>
2021-09-23 22:40:08 +0530

    model selection changes

--- mnist/mnist/plot_digits_classification.py
+++ mnist/mnist/plot_digits_classification.py
@@ -16,12 +16,15 @@ import matplotlib.pyplot as plt
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
-from sklearn.model_selection import train_test_split,GridSearchCV
-
-
+from sklearn.model_selection import train_test_split
+from joblib import dump, load
 from skimage import data, color
 from skimage.transform import rescale
 import numpy as np
+import pickle
+import os
+from util import pre_processing,split_data,test
+
 
 ###############################################################################
 # Digits dataset
@@ -62,77 +65,57 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 
 # flatten the images
 n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
 print("size of the Image is:")
 print(digits.images[0].shape)
-
-#resize_imgs=[]
-rescale_val = [1, 2, 3]
-test_split = [0.2, 0.3, 0.4]
+rescale_val = [1]
 gamma_val= [1, 0.1, 0.01, 0.001, 10]
+best_gamma = 0
+best_accuracy = 0
+best_f1_score = 0
+print("Gamma Value\tTest Accuracy\t\tTest F1 Score\t\tValidation Accuracy\tValidation F1 Score")
+for rescale_factor in rescale_val:
+  for val in gamma_val:
+    rescale_imgs = pre_processing(digits.images, rescale_factor)
+    rescale_imgs = np.array(rescale_imgs)
+    data = rescale_imgs.reshape((n_samples, -1))
+    # Create a classifier: a support vector classifier
+    clf = svm.SVC(gamma=val)
+    X_train,X_test,X_val,y_train,y_test,y_val = split_data(data, digits.target,0.3,0.1)
+    # Learn the digits on the train subset
+    clf.fit(X_train, y_train)
+    test_metrics = test(clf,X_test,y_test)
+    val_metrics = test(clf,X_val,y_val)
+    print("{}\t\t{}\t{}\t{}\t{}".format(val,test_metrics['Accuracy'],test_metrics['f1_score'],val_metrics['Accuracy'],val_metrics['f1_score']))
+    # print(test_metrics)
+    # best_gamma.append(val)
+    # best_accuracy.append(acc)
+    #Discard the models that yield random-like performance
 
-print("Image Size  Train-Test Split  Gamma val  Accuracy\t\t  F1 Score")
-for val in rescale_val:
-    resize_imgs = []
-    for img  in digits.images:
-        #resize_imgs= []
-        resize_imgs.append(rescale(img,val, anti_aliasing=False))
-    for test in test_split:
-        resize_imgs = np.array(resize_imgs)
-        data = resize_imgs.reshape((n_samples, -1))
-        for g in gamma_val:
-            # Create a classifier: a support vector classifier
-            clf = svm.SVC(gamma= g)
+    if val_metrics['Accuracy'] < 0.11:
+       print("Discard for {}".format(val))
+       continue
+    # save the model to disk
+    filename = 'finalized_model.sav'
+    pickle.dump(clf, open('/home/ranjeeta/miniconda3/mnist-example/mnist/mnist/model/finalized_model','wb')
+            )
+    print('Model is saved into to disk successfully Using Pickle')
+    if val_metrics['Accuracy'] > best_accuracy:
+       best_gamma = val
+       best_accuracy = val_metrics['Accuracy']
+       best_f1_score = val_metrics['f1_score']
 
-            # Split data into train and test subsets
-            X_train, X_test, y_train, y_test = train_test_split(
-                data, digits.target, test_size=test, shuffle=False)
+    # Load Model from the disk
+    my_model = pickle.load(open('/home/ranjeeta/miniconda3/mnist-example/mnist/mnist/model/finalized_model','rb'))
+    result = my_model.predict(X_val)
+    acc = metrics.accuracy_score(y_pred=result, y_true=y_val)
+    f1 = metrics.f1_score(y_pred=result, y_true=y_val, average="macro")
+    print("Validation Accuracy using saved Model",acc)
+    print("Validation F1 Score using saved Model",f1)
 
-            # Learn the digits on the train subset
-            clf.fit(X_train, y_train)
+print("The best gamma value is:",best_gamma)     
+print("The best Validation accuracy value is:",best_accuracy)  
+print("The best Validation F1 Score value is:",best_f1_score)  
 
-            # Predict the value of the digit on the test subset
-            predicted = clf.predict(X_test)
 
-            ###############################################################################
-            # Below we visualize the first 4 test samples and show their predicted
-            # digit value in the title.
 
-            _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-            for ax, image, prediction in zip(axes, X_test, predicted):
-                ax.set_axis_off()
-                image = image.reshape(resize_imgs[0].shape)
-                ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-                ax.set_title(f'Prediction: {prediction}')
-
-            ###############################################################################
-            # :func:`~sklearn.metrics.classification_report` builds a text report showing
-            # the main classification metrics.
-
-            # print(f"Classification report for classifier {clf}:\n"
-            #     f"{metrics.classification_report(y_test, predicted)}\n")
-
-            ###############################################################################
-            # We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-            # true digit values and the predicted digit values.
-
-            disp = metrics.plot_confusion_matrix(clf, X_test, y_test)
-            disp.figure_.suptitle("Confusion Matrix")
-            # print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-            acc = metrics.accuracy_score(y_pred=predicted, y_true=y_test)
-            f1 = metrics.f1_score(y_pred=predicted, y_true=y_test, average='macro')
-            print("{}x{}\t\t{}:{}\t{}\t{}\t{}".format(resize_imgs[0].shape[0], resize_imgs[0].shape[1], (1-test)*100,test*100,g,acc*100,f1*100))
-
-            plt.show()
-
-## Finding the best estimatior
-param_grid = { 'gamma': [1, 0.1, 0.01, 0.001, 10]}
-# Make grid search classifier
-
-clf_grid = GridSearchCV(svm.SVC(), param_grid, verbose=1)
-# Train the classifier
-
-clf_grid.fit(X_train, y_train)
-# clf = grid.best_estimator_()
-
-print("Best Estimators:\n", clf_grid.best_estimator_)
